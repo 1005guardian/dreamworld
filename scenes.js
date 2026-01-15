dbcb981a83e4e35d3c5cb00864a7612641c74bf0
1005guardian <98241949+1005guardian@users.noreply.github.com>
2026-01-14 18:06:25 -0800

    z

--- scenes.js
+++ scenes.js
@@ -24,11 +24,17 @@ window.gameData = {
         type: 'cinematic',
         hotspots: [
             // A smaller nested hotspot
-            { top: '1.3%', left: '38.8%', width: '8.6%', height: '8.6%', target: 'chestnut' }
+            { top: '1.3%', left: '38.8%', width: '8.6%', height: '8.6%', target: 'chestnut' },
+            { top: '30.0%', left: '43.8%', width: '10.2%', height: '10.2%', target: 'mushroom' }
         ]
     },
     'chestnut': {
         image: './chestnut.png',
+        type: 'cinematic',
+        hotspots: []
+    },
+    'mushroom': {
+        image: './mushroom.png',
         type: 'cinematic',
         hotspots: []
     },
