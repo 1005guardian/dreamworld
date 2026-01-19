aff458c21841fb37d7e0f296f4f11d047c63422e
1005guardian <98241949+1005guardian@users.noreply.github.com>
2026-01-19 13:00:13 -0800

    Update scenes.js

--- scenes.js
+++ scenes.js
@@ -55,7 +55,7 @@ window.gameData = {
         type: 'cinematic',
         hotspots: [
            { top: '51%', left: '59.8%', width: '6.2%', height: '6.2%', target: 'train' },
-           { top: '49.8%', left: '32.9%', width: '7.8%', height: '7.8%', target: 'electronic' },
+           { top: '49.9%', left: '33%', width: '7.6%', height: '7.6%', target: 'electronic' },
                    ]
     },
     'twohouses': {
